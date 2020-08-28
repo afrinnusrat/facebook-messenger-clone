286bec356d1047bb045d0fa855e7b446661ea849
wildenali <wildenwildenaliali@gmail.com>
2020-08-28 17:34:30 +0700

    add input and button tag

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <h1>Facebook Messenger Clone Coy</h1>
+      <input />
+      <button>Send Messege</button>
     </div>
   );
 }
