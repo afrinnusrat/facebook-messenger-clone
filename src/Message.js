3a84489febf4d387f6f9a8f2e1fea8a2800af17e
wildenali <wildenwildenaliali@gmail.com>
2020-08-28 21:14:47 +0700

    ganti tag di Message.js jadi tag material-ui

--- src/Message.js
+++ src/Message.js
@@ -1,10 +1,19 @@
 import React from 'react'
+import { Card, CardContent, Typography } from '@material-ui/core'
 
 function Message(props) {
     return (
-        <div>
-            <h2>{props.username}: {props.text}</h2>
-        </div>
+        <Card>
+            <CardContent>
+                <Typography
+                    color="white"
+                    variant="h5"
+                    component="h5"
+                >
+                    {props.username}: {props.text}
+                </Typography>
+            </CardContent>
+        </Card>
     )
 }
 
