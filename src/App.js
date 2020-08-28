c2b4578641025d1abfbf1412528caca62002eb82
wildenali <wildenwildenaliali@gmail.com>
2020-08-28 18:29:53 +0700

    menampilkan data yg ada di messages ke halaman, pake <p> tag

--- src/App.js
+++ src/App.js
@@ -4,21 +4,30 @@ import './App.css';
 function App() {
   
   const [input, setInput] = useState('');
-  const [message, setMessage] = useState('');
+  const [messages, setMessages] = useState(['Hai', 'Mantap']);
   // console.log(input);
+  // console.log(message);
 
   const sendMessage = (event) => {
     // all the logic to send
-    setMessage([...message, input]);
+    setMessages([...messages, input]);
     setInput('');
   }
-  console.log(message);
   
   return (
     <div className="App">
       <h1>Facebook Messenger Clone Coy</h1>
       <input value={input} onChange={event => setInput(event.target.value)} />
       <button onClick={sendMessage}>Send Messege</button>
+
+      {/* Message themselves */}
+
+      {
+        messages.map(message => (
+          <p>{message}</p>
+        ))
+      }
+
     </div>
   );
 }
