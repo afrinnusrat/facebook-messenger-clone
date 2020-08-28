8e774445ada2cee66edb2573905fd8e22d50748c
wildenali <wildenwildenaliali@gmail.com>
2020-08-28 19:23:29 +0700

    tambah useState username

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Button, FormControl, Input, InputLabel } from '@material-ui/core';
 import './App.css';
 import Message from './Message';
@@ -7,8 +7,18 @@ function App() {
   
   const [input, setInput] = useState('');
   const [messages, setMessages] = useState(['Hai', 'Mantap']);
+  const [username, setUsername] = useState('');
   // console.log(input);
   // console.log(message);
+
+  // useEffect, run code on a condition in REACT
+  useEffect(() => {
+    // run code here...
+    // if its blank inside [] depedencies, this code runs ONCE whene the app component loads
+    // const name = prompt('Please enter your name');
+    setUsername(prompt('Please enter your name'));
+  }, [])  // disini kondisinya apa dulu
+  console.log(username);
 
   const sendMessage = (event) => {
     // all the logic to send 
@@ -20,6 +30,7 @@ function App() {
   return (
     <div className="App">
       <h1>Facebook Messenger Clone Coy</h1>
+      <h2>Welcome {username}</h2>
 
       <form>
         <FormControl>
